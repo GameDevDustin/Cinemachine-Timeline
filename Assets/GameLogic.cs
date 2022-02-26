64d726f8aae9dfeeb3ef5a96878d3a3055aa9533
D Spears <37002982+Xel3@users.noreply.github.com>
2022-02-25 20:58:45 -0600

    Switch Cameras in code
    Switch camera in code with R key

--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -6,44 +6,62 @@ using Cinemachine;
 public class GameLogic : MonoBehaviour
 {
     [SerializeField]
-    private Transform[] vCamsTransforms;
+    private Transform[] _vCamsTransforms;
     [SerializeField]
-    private CinemachineVirtualCamera[] vCams = new CinemachineVirtualCamera[5];
-    private CinemachineVirtualCamera switchToVCam;
+    private CinemachineVirtualCamera[] _vCams = new CinemachineVirtualCamera[5];
+    private CinemachineVirtualCamera _switchToVCam;
+    [SerializeField]
+    private Transform[] _targetGOs;
+    private int _currTarget = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         //Assign VCam components to array
-        vCams[0] = vCamsTransforms[0].GetComponent<CinemachineVirtualCamera>();
-        vCams[1] = vCamsTransforms[1].GetComponent<CinemachineVirtualCamera>();
-        vCams[1].gameObject.SetActive(false);
-        vCams[2] = vCamsTransforms[2].GetComponent<CinemachineVirtualCamera>();
-        vCams[2].gameObject.SetActive(false);
-        vCams[3] = vCamsTransforms[3].GetComponent<CinemachineVirtualCamera>();
-        vCams[3].gameObject.SetActive(false);
-        vCams[4] = vCamsTransforms[4].GetComponent<CinemachineVirtualCamera>();
-        vCams[4].gameObject.SetActive(false);
+        _vCams[0] = _vCamsTransforms[0].GetComponent<CinemachineVirtualCamera>();
+        _vCams[1] = _vCamsTransforms[1].GetComponent<CinemachineVirtualCamera>();
+        _vCams[1].gameObject.SetActive(false);
+        _vCams[2] = _vCamsTransforms[2].GetComponent<CinemachineVirtualCamera>();
+        _vCams[2].gameObject.SetActive(false);
+        _vCams[3] = _vCamsTransforms[3].GetComponent<CinemachineVirtualCamera>();
+        _vCams[3].gameObject.SetActive(false);
+        _vCams[4] = _vCamsTransforms[4].GetComponent<CinemachineVirtualCamera>();
+        _vCams[4].gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Change targets for _vCams[0]
+        if (Input.GetKeyDown(KeyCode.R) == true)
+        {
+            //Set new _currTarget
+            if (_currTarget == 1)
+            {
+                _currTarget = 2;
+                _vCams[0].LookAt = _targetGOs[1];
+            } else
+            {
+                _currTarget = 1;
+                _vCams[0].LookAt = _targetGOs[0];
+            }            
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha1) == true)
         {
             //Enable VCam 1
-            switchToVCam = vCams[0];
-            switchToVCam.gameObject.SetActive(true);
-            switchToVCam.Priority = 100;
+            _switchToVCam = _vCams[0];
+            _switchToVCam.gameObject.SetActive(true);
+            _switchToVCam.Priority = 100;
             ResetVCamsPriorities(0);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2) == true)
         {
             //Enable VCam 2
-            switchToVCam = vCams[1];
-            switchToVCam.gameObject.SetActive(true);
-            switchToVCam.Priority = 100;
+            _switchToVCam = _vCams[1];
+            _switchToVCam.gameObject.SetActive(true);
+            _switchToVCam.Priority = 100;
             ResetVCamsPriorities(1);
         }
 
@@ -51,27 +69,27 @@ public class GameLogic : MonoBehaviour
         {
 
             //Enable VCam 3
-            switchToVCam = vCams[2];
-            switchToVCam.gameObject.SetActive(true);
-            switchToVCam.Priority = 100;
+            _switchToVCam = _vCams[2];
+            _switchToVCam.gameObject.SetActive(true);
+            _switchToVCam.Priority = 100;
             ResetVCamsPriorities(2);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha4) == true)
         {
             //Enable VCam 4
-            switchToVCam = vCams[3];
-            switchToVCam.gameObject.SetActive(true);
-            switchToVCam.Priority = 100;
+            _switchToVCam = _vCams[3];
+            _switchToVCam.gameObject.SetActive(true);
+            _switchToVCam.Priority = 100;
             ResetVCamsPriorities(3);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha5) == true)
         {
             //Enable VCam 5
-            switchToVCam = vCams[4];
-            switchToVCam.gameObject.SetActive(true);
-            switchToVCam.Priority = 100;
+            _switchToVCam = _vCams[4];
+            _switchToVCam.gameObject.SetActive(true);
+            _switchToVCam.Priority = 100;
             ResetVCamsPriorities(4);
         }
     }
@@ -82,45 +100,45 @@ public class GameLogic : MonoBehaviour
         {
             case 0:
                 for (int i = 1; i < 5; i++) {
-                    vCams[i].Priority = 10;
-                    vCams[i].gameObject.SetActive(false);
+                    _vCams[i].Priority = 10;
+                    _vCams[i].gameObject.SetActive(false);
                 }
                 break;
             case 1:
-                vCams[0].Priority = 10;
-                vCams[0].gameObject.SetActive(false);
-                vCams[2].Priority = 10;
-                vCams[2].gameObject.SetActive(false);
-                vCams[3].Priority = 10;
-                vCams[3].gameObject.SetActive(false);
-                vCams[4].Priority = 10;
-                vCams[4].gameObject.SetActive(false);
+                _vCams[0].Priority = 10;
+                _vCams[0].gameObject.SetActive(false);
+                _vCams[2].Priority = 10;
+                _vCams[2].gameObject.SetActive(false);
+                _vCams[3].Priority = 10;
+                _vCams[3].gameObject.SetActive(false);
+                _vCams[4].Priority = 10;
+                _vCams[4].gameObject.SetActive(false);
                 break;
             case 2:
-                vCams[0].Priority = 10;
-                vCams[0].gameObject.SetActive(false);
-                vCams[1].Priority = 10;
-                vCams[1].gameObject.SetActive(false);
-                vCams[3].Priority = 10;
-                vCams[3].gameObject.SetActive(false);
-                vCams[4].Priority = 10;
-                vCams[4].gameObject.SetActive(false);
+                _vCams[0].Priority = 10;
+                _vCams[0].gameObject.SetActive(false);
+                _vCams[1].Priority = 10;
+                _vCams[1].gameObject.SetActive(false);
+                _vCams[3].Priority = 10;
+                _vCams[3].gameObject.SetActive(false);
+                _vCams[4].Priority = 10;
+                _vCams[4].gameObject.SetActive(false);
                 break;
             case 3:
-                vCams[0].Priority = 10;
-                vCams[0].gameObject.SetActive(false);
-                vCams[1].Priority = 10;
-                vCams[1].gameObject.SetActive(false);
-                vCams[2].Priority = 10;
-                vCams[2].gameObject.SetActive(false);
-                vCams[4].Priority = 10;
-                vCams[4].gameObject.SetActive(false);
+                _vCams[0].Priority = 10;
+                _vCams[0].gameObject.SetActive(false);
+                _vCams[1].Priority = 10;
+                _vCams[1].gameObject.SetActive(false);
+                _vCams[2].Priority = 10;
+                _vCams[2].gameObject.SetActive(false);
+                _vCams[4].Priority = 10;
+                _vCams[4].gameObject.SetActive(false);
                 break;
             case 4:
                 for (int i = 3; i > -1; i--)
                 {
-                    vCams[i].Priority = 10;
-                    vCams[i].gameObject.SetActive(false);
+                    _vCams[i].Priority = 10;
+                    _vCams[i].gameObject.SetActive(false);
                 }
                 break;
             default:
